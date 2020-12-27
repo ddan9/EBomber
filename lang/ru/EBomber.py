644853ffc9e766b0bac933fe0bf86630d89e3eec
Danil Reddnine <76552654+ddan9@users.noreply.github.com>
2020-12-27 15:51:10 +0300

    Add files via upload

--- lang/ru/EBomber.py
+++ lang/ru/EBomber.py
@@ -17,14 +17,12 @@ init()
 
 #Баннер старта 
 os.system("clear")
-os.system("CLS")
 start()
 input()
 
 
 #Баннер cПАМЕРА
 os.system("clear")
-os.system("CLS")
 
 if oso == "posix":
     banner1an()
@@ -38,38 +36,42 @@ else:
     banner4()
 
 
-message1 = "\n Изменения применены !"
-message8 = """    \n         Разработчики:  
+message1 = "\n Изменения применены!"
+message8 = """    \n Разработчики:  
                   \n                                      
                   \n Создатель идеи/Разработчик:
                   \n \033[94mИван Зайцев\033[0m - https://vk.com/ivan_vzlom300 
                   \n                                      
-                  \n \033[33mНад визуальными эффектами\Анимации работали:\033[0m 
+                  \n \033[33mСоздатель эффектов/анимации:\033[0m 
                   \n \033[94mДима Чернышов\033[0m - https://vk.com/psih.odinochka  
-                  \n                                      
+                  \n                                     
+                  \n \033[33mДистрибьютор/фиксер:\033[0m 
+                  \n \033[94mddan9\033[0m - https://github.com/ddan9 
+                  \n    
+                                  
                                                                       """
 
 
 print(Fore.GREEN)
 time.sleep(1)
-E = input("\n Хотите попасть в настройки ? [1-(Yes)/\033[31m2-(No)\033[0m\033[32m]>>\033[94m")
+E = input("\n Хотите попасть в настройки? (1 - Да | \033[31m2 - Нет\033[0m\033[32m) > \033[94m")
 if E == str(1):
     print(Fore.GREEN)
     print("\n          [НАСТРОЙКИ] ")
     print("\n \033[94m[0] Разработчики \033[0m                             ")
-    print("\n \033[32m[1] Настройка времени показа баннера (в сек.)\033[0m  ")
-    print("\n \033[32m[2] Выйти\033[0m ")
+    print("\n \033[32m[1] Время показа баннера (Сек)\033[0m  ")
+    print("\n \033[32m[2] Выход\033[0m ")
     time.sleep(2)
     print(Fore.GREEN)
-    p = input("\n [>>] ")
+    p = input("\n [>] ")
     
     if p == str(1):
-        kol2 =  input("\n Хорошо введите время не больше 20 секунд !>> ")
+        kol2 =  input("\n Введите время не более 20! > ")
         for char in message1:
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(0.01)
-        p = input("\n Что бы выйти напишите (2) >> ")
+        p = input("\n Чтобы выйти введите (2) > ")
 
     if p == str(0):
         print(Fore.YELLOW)
@@ -78,11 +80,10 @@ if E == str(1):
             sys.stdout.flush()
             time.sleep(0.01)
         print(Fore.GREEN)
-        p = input("\n Что бы выйти напишите (2) >> ")
+        p = input("\n Чтобы выйти введите (2) > ")
 
     if p == str(2):
         os.system("clear")
-        os.system("CLS")
         if oso == "posix":
             banner3()
         else:
@@ -90,7 +91,6 @@ if E == str(1):
 
 if E == str(2):
     os.system("clear")
-    os.system("CLS")
     if oso == "posix":
         banner3()
     else:
@@ -105,7 +105,8 @@ def banner_atack2():
 			┃┗━━╋┓┏╋━━┳┻┓┏┛
 			┗━━┓┃┃┃┃┏┓┃┏┫┃
 			┃┗━┛┃┃┗┫┏┓┃┃┃┗┓
-			┗━━━┛┗━┻┛┗┻┛┗━┛....."""
+			┗━━━┛┗━┻┛┗┻┛┗━┛...
+"""
   print(baner)
   print (" \033[31m[                    ]\033[33m \033[94m5%\033[0m")
   time.sleep(0.1)
@@ -150,7 +151,7 @@ def banner_atack():
 
 
 R = 0
-message7 = "\033[94m \n ЧТо бы вЫйти из прОграМмы нАжмиТе Ctrl + C ! \033[0m"
+message7 = "\033[94m \n Чтобы выйти из программы, нажмите Ctrl + C\033[0m"
 message5 = "\033[94m \n ЧТо бы вЫйти из прОграМмы нАжмиТе Ctrl + C иЛи на КРЕСТИК в пРавоМ верХнем угЛу. \033[0m"
 if oso == "posix":
     for char in message7:
@@ -165,8 +166,8 @@ else:
         time.sleep(0.01)
 
 print(Fore.RED)
-_phone = input('\n \033[31mВвEди-те ноMер (Без +)>\033[0m\033[94m')
-message4 =  "Атака началась на ||" + str(_phone)+ "||\033[94mDDos-SMS-Attack\033[0m"
+_phone = input('\n \033[31mВведите номер (Без +) > \033[0m\033[94m')
+message4 =  "Начинаем атаку на || " + str(_phone)+ " || \033[94mDDOS SMS Атака\033[0m \n"
 
 if _phone[0] == '+':
 
@@ -180,15 +181,14 @@ if _phone[0] == '9':
 
     _phone = '7'+_phone
 
-kol = input("\n \033[31mВрЕмя аТакИ в сЕк.(Ставить не меньше 10 , 0 - бесконечно)>\033[0m\033[94m ")
+kol = input("\n \033[31mВремя атаки (Введите больше 30 | 0 - бесконечно) > \033[0m\033[94m ")
 if kol == str(0):
     kol = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 start_time = time.time() 
 CLOSE_AFTER = kol
-call = input("\n \033[31mTип атаки (1 - Атака звонками , 2 -Атака СМС)>\033[0m\033[94m")
+call = input("\n \033[31mTип атаки (1 - Атака звонками (НЕ РАБОТАЕТ) | 2 - SMS Атака) > \033[0m\033[94m")
 if call == str(1):
     os.system("clear")
-    os.system("CLS")
 
     if oso == "posix":
         banner_atack2()
@@ -241,8 +241,7 @@ if call == str(1):
 if call == str(2):
     start_time = time.time() 
     CLOSE_AFTER = kol
-    os.system("clear")
-    os.system("CLS")   
+    os.system("clear") 
     if oso == "posix":
         banner_atack2()
     else:
